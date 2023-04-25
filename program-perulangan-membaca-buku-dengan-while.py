bac4fc519f5661f1c357e3a0f7e6105bc5a1ce68
chaturap <catur.ap@gmail.com>
2023-04-25 18:24:53 +0700

    perulangan dengan while

---
+++ program-perulangan-membaca-buku-dengan-while.py
@@ -0,0 +1,19 @@
+"""
+Program perulangan membaca buku dengan while
+"""
+
+jumlah_buku = 10
+print('Ibu berkata "Baca semua bukumu"')
+print(f"jumlah_buku {jumlah_buku} ")
+
+jumlah_buku_yang_sudah_dibaca = 0
+
+print(f"jumlah buku yang sudah dibaca {jumlah_buku_yang_sudah_dibaca} ")
+
+
+while jumlah_buku_yang_sudah_dibaca < jumlah_buku:
+    jumlah_buku_yang_sudah_dibaca = jumlah_buku_yang_sudah_dibaca + 1
+    print(f"Buke ke {jumlah_buku_yang_sudah_dibaca}")
+
+#i+1
+
