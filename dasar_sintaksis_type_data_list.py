d3840b3c5cb4bafd4c3b8586b4ada6722f462d6c
chaturap <catur.ap@gmail.com>
2023-04-25 22:46:22 +0700

    tipe data list dengan del, list comp

--- dasar_sintaksis_type_data_list.py
+++ dasar_sintaksis_type_data_list.py
@@ -51,31 +51,3 @@ daftar_buku = ['how to influence people', 'habbit', 'Muhammad AL fatih']
 daftar_buku.pop(-1)
 for i in range(0, len(daftar_buku)):
     print(daftar_buku[i])
-
-# hapus element dengan perintah del
-print("hapus element tertentu dengan dell, semua")
-daftar_buku = ['how to influence people', 'habbit', 'Muhammad AL fatih']
-del daftar_buku[:]
-for i in range(0, len(daftar_buku)):
-    print(daftar_buku[i])
-
-# hapus element dengan perintah del dari index ke 0 sebanyak 1
-print("hapus element tertentu dengan dell, dari index ke 0 sebanyak 1")
-daftar_buku = ['how to influence people', 'habbit', 'Muhammad AL fatih']
-del daftar_buku[0:1]
-for i in range(0, len(daftar_buku)):
-    print(daftar_buku[i])
-
-# hapus element dengan perintah del dengan step
-print("hapus element tertentu dengan dell, minus")
-daftar_buku = ['how to influence people', 'habbit', 'Muhammad AL fatih','karimah']
-del daftar_buku[0:-2]
-for i in range(0, len(daftar_buku)):
-    print(daftar_buku[i])
-
-# hapus element dengan perintah del dengan step
-print("hapus element tertentu dengan dell, dengan step")
-daftar_buku = ['how to influence people', 'habbit', 'Muhammad AL fatih','karimah']
-del daftar_buku[0::2] #index:sebanyak berapa:step
-for i in range(0, len(daftar_buku)):
-    print(daftar_buku[i])
