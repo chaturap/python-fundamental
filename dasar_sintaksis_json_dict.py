b22c045525b9b3fcbf977b77b2040b83def14005
chaturap <catur.ap@gmail.com>
2023-04-26 06:32:26 +0700

    json

---
+++ dasar_sintaksis_json_dict.py
@@ -0,0 +1,41 @@
+users = {
+"id": 1,
+"name": "Leanne Graham",
+"username": "Bret",
+"email": "Sincere@april.biz",
+"address":
+    {
+    "street": "Kulas Light",
+    "suite": "Apt. 556",
+    "city": "Gwenborough",
+    "zipcode": "92998-3874",
+        "geo": {
+            "lat": "-37.3159",
+            "lng": "81.1496"
+        }
+    }
+}
+
+print(users)
+print("id :", users["id"])
+print("Name :", users["name"])
+print("Username :", users["username"])
+print("Email : ", users["email"])
+print("street : ", users["address"]["street"])
+print("suite : ", users["address"]["suite"])
+print("city : ", users["address"]["city"])
+print("zipcode : ", users["address"]["zipcode"])
+print("Lat : ", users["address"]["geo"]["lat"] )
+print("Lng : ", users["address"]["geo"]["lng"])
+
+print(type(users))
+import
+print("cetak dengan json dump")
+result = json.dumps(users)
+print(result)
+print(type(result))
+
+
+print("cetak json ke file")
+with open('result.json', 'w') as file:
+    json.dump(users, file)
