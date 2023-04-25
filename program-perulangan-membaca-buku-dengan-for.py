c3b735e29b7245b1213b303560dfbd7e24f4dbb7
chaturap <catur.ap@gmail.com>
2023-04-25 18:25:06 +0700

    perulangan dengan for

---
+++ program-perulangan-membaca-buku-dengan-for.py
@@ -0,0 +1,17 @@
+"""
+Program perulangan membaca buku
+"""
+
+jumlah_buku = 10
+print('Ibu berkata "Baca semua bukumu"')
+print(f"jumlah_buku {jumlah_buku} ")
+
+jumlah_buku_yang_sudah_dibaca = 0
+
+print(f"jumlah_buku {jumlah_buku} ")
+
+i = 1
+for i in range(1,jumlah_buku+1):
+    print(f"baris ke {i}")
+#i+1
+
