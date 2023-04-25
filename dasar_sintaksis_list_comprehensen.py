597a1781184bfd5d2e510dcd3ee463504f75fd49
chaturap <catur.ap@gmail.com>
2023-04-25 22:56:14 +0700

    copy isi list

--- dasar_sintaksis_list_comprehensen.py
+++ dasar_sintaksis_list_comprehensen.py
@@ -26,3 +26,15 @@ daftar_buku = ['how to influence people', 'habbit', 'Muhammad AL fatih','karimah
 del daftar_buku[0::2] #index:sebanyak berapa:step
 for i in range(0, len(daftar_buku)):
     print(daftar_buku[i])
+
+# mengcopy list
+daftar_buku = ['how to influence people', 'habbit', 'Muhammad AL fatih', 'karimah']
+print("mengcopy list")
+daftar_buku_baru = daftar_buku[:]
+del daftar_buku[:]
+for i in range(0, len(daftar_buku)):
+    print(daftar_buku[i])
+    
+print("tampilkan daftar buku baru")
+for i in range(0, len(daftar_buku_baru)):
+    print(daftar_buku_baru[i])
